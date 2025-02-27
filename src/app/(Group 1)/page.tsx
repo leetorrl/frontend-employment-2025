0d29762c6b8bf3468b18eae3173f19c9c4184a59
leetorrl <leetorrl123@gmail.com>
2025-02-27 18:14:15 +0900

    0227 - count버튼 추가

--- src/app/(Group 1)/page.tsx
+++ src/app/(Group 1)/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 import Link from "next/link";
-
+import { useState } from "react";
 export default function Home() {
+
+  const [count, setCount] = useState(0);
+
+  const upcount = () => {
+
+    setCount(count+1);
+  }
+
   return <section>
     <div>
       <h1>홈웹</h1>
+      <h1>{count}</h1>
+      <button onClick={upcount}>Click!!</button>
+
+<br />
 <Link href="/sign-in" className="bg-red-500">로그인 라우터</Link>
     </div>
   </section>;
