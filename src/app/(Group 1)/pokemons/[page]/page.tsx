e79aa05bce67ba8aec618b440b414e1210010819
leetorrl <leetorrl123@gmail.com>
2025-02-28 15:57:21 +0900

    0228 - 페이지 네이션 추가

--- src/app/(Group 1)/pokemons/[page]/page.tsx
+++ src/app/(Group 1)/pokemons/[page]/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ThemeProvider from "../../components/ThemeProvider";
+import ThemeProvider from "../../../components/ThemeProvider";
 import Image from "next/image";
 
 interface Pokemon {
@@ -7,27 +7,36 @@ interface Pokemon {
   image: string;
 }
 
+
 async function fetchPokemons(offset: number) {
   const res = await fetch(`http://localhost:3000/api/pokemon?limit=20&offset=${offset}`);
   const data = await res.json();
   return data;
 }
 
-export default async function Pokemons({ params }: { params?: { page?: string } }) {
-  const currentPage = Number(params?.page) || 1; // undefined 대비
+export async function generateStaticParams() {
+    const totalPokemons = 1298;
+    const totalPages = Math.ceil(totalPokemons / 20);
+    
+    return Array.from({ length: totalPages }, (_, i) => ({ page: String(i + 1) }));
+  }
+
+export default async function PokemonPage({ params }: { params: { page: string } }) {
+  const currentPage = Number(params?.page) || 1;
   const offset = (currentPage - 1) * 20;
-  const initialPokemons = await fetchPokemons(offset);
-  const totalPokemons = 1298;
-  const totalPages = Math.ceil(totalPokemons / 20);
+  const pokemons: Pokemon[] = await fetchPokemons(offset);
+
+  const totalPokemons = 1298; 
+  const totalPages = Math.ceil(totalPokemons / 20); 
 
   return (
     <ThemeProvider>
       <section className="flex justify-center items-center w-full">
         <div className="w-[2540px] overflow-x-auto">
-          <h1 className="text-center text-2xl mb-4">Pokémon List</h1>
-          
+          <h1 className="text-center text-2xl mb-4">Pokémon List (Page {currentPage})</h1>
+
           <div className="grid grid-cols-4 gap-4 min-w-max">
-            {initialPokemons.map((pokemon: Pokemon) => (
+            {pokemons.map((pokemon) => (
               <div key={pokemon.name} className="flex flex-col items-center mb-6">
                 <div className="border-2 flex items-center justify-center w-[600px] h-[800px]">
                   <Image
@@ -45,7 +54,7 @@ export default async function Pokemons({ params }: { params?: { page?: string } 
 
           <div className="text-center mt-4">
             <div className="flex justify-center gap-2">
-              {Array.from({ length: totalPages }, (_, index) => (
+              {[...Array(totalPages)].map((_, index) => (
                 <Link key={index} href={`/pokemons/${index + 1}`}>
                   <button
                     className={`px-4 py-2 border rounded-lg ${
